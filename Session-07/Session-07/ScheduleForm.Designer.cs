4d0a2a4cdb55f0c3c6332f0de3096f6ed1997f32
siderism <michaelsid4@gmail.com>
2022-03-11 14:52:39 +0200

    updated ScheduleForm functionality

--- Session-07/Session-07/ScheduleForm.Designer.cs
+++ Session-07/Session-07/ScheduleForm.Designer.cs
@@ -36,10 +36,16 @@
             this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
             this.textEdit2 = new DevExpress.XtraEditors.TextEdit();
             this.dateEdit1 = new DevExpress.XtraEditors.DateEdit();
+            this.newScheduleButton = new DevExpress.XtraEditors.SimpleButton();
+            this.deleteScheduleButton = new DevExpress.XtraEditors.SimpleButton();
+            this.updateScheduleButton = new DevExpress.XtraEditors.SimpleButton();
+            this.closeButton = new DevExpress.XtraEditors.SimpleButton();
+            this.scheduleListBox = new DevExpress.XtraEditors.ListBoxControl();
             ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dateEdit1.Properties.CalendarTimeProperties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dateEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.scheduleListBox)).BeginInit();
             this.SuspendLayout();
             // 
             // saveButton
@@ -62,7 +68,7 @@
             // 
             // labelControl1
             // 
-            this.labelControl1.Location = new System.Drawing.Point(3, 50);
+            this.labelControl1.Location = new System.Drawing.Point(495, 52);
             this.labelControl1.Name = "labelControl1";
             this.labelControl1.Size = new System.Drawing.Size(48, 13);
             this.labelControl1.TabIndex = 2;
@@ -70,7 +76,7 @@
             // 
             // labelControl2
             // 
-            this.labelControl2.Location = new System.Drawing.Point(3, 77);
+            this.labelControl2.Location = new System.Drawing.Point(495, 79);
             this.labelControl2.Name = "labelControl2";
             this.labelControl2.Size = new System.Drawing.Size(60, 13);
             this.labelControl2.TabIndex = 3;
@@ -78,7 +84,7 @@
             // 
             // labelControl3
             // 
-            this.labelControl3.Location = new System.Drawing.Point(3, 117);
+            this.labelControl3.Location = new System.Drawing.Point(495, 119);
             this.labelControl3.Name = "labelControl3";
             this.labelControl3.Size = new System.Drawing.Size(45, 13);
             this.labelControl3.TabIndex = 4;
@@ -86,14 +92,14 @@
             // 
             // textEdit1
             // 
-            this.textEdit1.Location = new System.Drawing.Point(70, 47);
+            this.textEdit1.Location = new System.Drawing.Point(562, 49);
             this.textEdit1.Name = "textEdit1";
             this.textEdit1.Size = new System.Drawing.Size(100, 20);
             this.textEdit1.TabIndex = 5;
             // 
             // textEdit2
             // 
-            this.textEdit2.Location = new System.Drawing.Point(70, 70);
+            this.textEdit2.Location = new System.Drawing.Point(562, 72);
             this.textEdit2.Name = "textEdit2";
             this.textEdit2.Size = new System.Drawing.Size(100, 20);
             this.textEdit2.TabIndex = 6;
@@ -101,7 +107,7 @@
             // dateEdit1
             // 
             this.dateEdit1.EditValue = null;
-            this.dateEdit1.Location = new System.Drawing.Point(70, 110);
+            this.dateEdit1.Location = new System.Drawing.Point(562, 112);
             this.dateEdit1.Name = "dateEdit1";
             this.dateEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
             new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
@@ -110,11 +116,60 @@
             this.dateEdit1.Size = new System.Drawing.Size(100, 20);
             this.dateEdit1.TabIndex = 7;
             // 
+            // newScheduleButton
+            // 
+            this.newScheduleButton.Location = new System.Drawing.Point(468, 414);
+            this.newScheduleButton.Name = "newScheduleButton";
+            this.newScheduleButton.Size = new System.Drawing.Size(75, 23);
+            this.newScheduleButton.TabIndex = 8;
+            this.newScheduleButton.Text = "New";
+            this.newScheduleButton.Click += new System.EventHandler(this.newScheduleButton_Click);
+            // 
+            // deleteScheduleButton
+            // 
+            this.deleteScheduleButton.Location = new System.Drawing.Point(549, 414);
+            this.deleteScheduleButton.Name = "deleteScheduleButton";
+            this.deleteScheduleButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteScheduleButton.TabIndex = 9;
+            this.deleteScheduleButton.Text = "Delete";
+            this.deleteScheduleButton.Click += new System.EventHandler(this.deleteScheduleButton_Click);
+            // 
+            // updateScheduleButton
+            // 
+            this.updateScheduleButton.Location = new System.Drawing.Point(630, 414);
+            this.updateScheduleButton.Name = "updateScheduleButton";
+            this.updateScheduleButton.Size = new System.Drawing.Size(75, 23);
+            this.updateScheduleButton.TabIndex = 10;
+            this.updateScheduleButton.Text = "Update";
+            this.updateScheduleButton.Click += new System.EventHandler(this.updateScheduleButton_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(713, 414);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 11;
+            this.closeButton.Text = "Close";
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // scheduleListBox
+            // 
+            this.scheduleListBox.Location = new System.Drawing.Point(3, 48);
+            this.scheduleListBox.Name = "scheduleListBox";
+            this.scheduleListBox.Size = new System.Drawing.Size(459, 389);
+            this.scheduleListBox.TabIndex = 12;
+            this.scheduleListBox.SelectedIndexChanged += new System.EventHandler(this.scheduleListBox_SelectedIndexChanged);
+            // 
             // ScheduleForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.scheduleListBox);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.updateScheduleButton);
+            this.Controls.Add(this.deleteScheduleButton);
+            this.Controls.Add(this.newScheduleButton);
             this.Controls.Add(this.dateEdit1);
             this.Controls.Add(this.textEdit2);
             this.Controls.Add(this.textEdit1);
@@ -129,6 +184,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dateEdit1.Properties.CalendarTimeProperties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dateEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.scheduleListBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -144,5 +200,10 @@
         private DevExpress.XtraEditors.TextEdit textEdit1;
         private DevExpress.XtraEditors.TextEdit textEdit2;
         private DevExpress.XtraEditors.DateEdit dateEdit1;
+        private DevExpress.XtraEditors.SimpleButton newScheduleButton;
+        private DevExpress.XtraEditors.SimpleButton deleteScheduleButton;
+        private DevExpress.XtraEditors.SimpleButton updateScheduleButton;
+        private DevExpress.XtraEditors.SimpleButton closeButton;
+        private DevExpress.XtraEditors.ListBoxControl scheduleListBox;
     }
 }
--- Session-07/Session-07/ScheduleForm.cs
+++ Session-07/Session-07/ScheduleForm.cs
@@ -16,9 +16,12 @@ namespace Session_07
     public partial class ScheduleForm : Form
     {
         private const string SCHEDULE_FILE = "schedules.json";
+        private Schedule _schedule;
+        public List<Schedule> Schedules { get; set; }
         public ScheduleForm()
         {
             InitializeComponent();
+            Schedules = new List<Schedule>();
         }
 
         private void saveButton_Click(object sender, EventArgs e)
@@ -33,20 +36,85 @@ namespace Session_07
 
         private void SaveSchedulesToJSON()
         {
-            var schedule = new Schedule()
-            {
-                CourseID = Guid.Parse(textEdit1.EditValue.ToString()),
-                ProfessorID = Guid.Parse(textEdit2.EditValue.ToString()),
-                Callendar = DateTime.Parse(dateEdit1.EditValue.ToString()),
-            };
-            string json = new JavaScriptSerializer().Serialize(schedule);
+            string json = new JavaScriptSerializer().Serialize(Schedules);
             File.WriteAllText(SCHEDULE_FILE, json);
         }
 
         private void LoadSchedules()
         {
             string data = File.ReadAllText(SCHEDULE_FILE);
-            Schedule schedule= new JavaScriptSerializer().Deserialize<Schedule>(data);
+            Schedules = new JavaScriptSerializer().Deserialize<List<Schedule>>(data);
+            ShowList();
+        }
+
+        private void newScheduleButton_Click(object sender, EventArgs e)
+        {
+            CreateSchedule();
+            ShowList();
+        }
+
+        private void deleteScheduleButton_Click(object sender, EventArgs e)
+        {
+            DeleteSchedule();
+            ShowList();
+        }
+
+        private void updateScheduleButton_Click(object sender, EventArgs e)
+        {
+            UpdateSchedule();
+            ShowList();
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            SaveSchedulesToJSON();
+            this.Close();
+        }
+
+        private void scheduleListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (scheduleListBox.SelectedIndex != -1)
+            {
+                _schedule = Schedules.ElementAt(Convert.ToInt32(scheduleListBox.SelectedIndex));
+                textEdit1.Text = _schedule.CourseID.ToString();
+                textEdit2.Text = _schedule.ProfessorID.ToString();
+                dateEdit1.DateTime = DateTime.Parse(_schedule.Callendar.ToString());
+            }
+        }
+
+        private void CreateSchedule()
+        {
+            _schedule = new Schedule()
+            {
+                CourseID = Guid.Parse(textEdit1.EditValue.ToString()),
+                ProfessorID = Guid.Parse(textEdit2.EditValue.ToString()),
+                Callendar = DateTime.Parse(dateEdit1.EditValue.ToString())
+            };
+            Schedules.Add(_schedule);
+        }
+
+        private void DeleteSchedule()
+        {
+            Schedules.Remove(_schedule);
+        }
+
+        private void UpdateSchedule()
+        {
+            _schedule.CourseID = Guid.Parse(textEdit1.EditValue.ToString());
+            _schedule.ProfessorID = Guid.Parse(textEdit2.EditValue.ToString());
+            _schedule.Callendar = DateTime.Parse(dateEdit1.EditValue.ToString());
+        }
+
+        private void ShowList()
+        {
+            scheduleListBox.Items.Clear();
+            foreach (var schedule in Schedules)
+            {
+                if (schedule != null)
+                {
+                    scheduleListBox.Items.Add($"Callendar - {schedule.Callendar}");
+                }
+            }
         }
     }
 }
