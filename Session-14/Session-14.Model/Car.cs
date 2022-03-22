e086bb16a6a7c0a900359b7c7495129ef856ffe0
siderism <michaelsid4@gmail.com>
2022-03-22 13:24:27 +0200

    created new solution, added models

---
+++ Session-14/Session-14.Model/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_14.Model
+{
+    public class Car : Item
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string CarRegNumber { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return $"{Brand} {Model}";
+            }
+        }
+        public Car()
+        {
+
+        }
+    }
+}
---
+++ Session-14/Session-14.Model/MonthlyLedger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_14.Model
+{
+    public class MonthlyLedger
+    {
+        public int Year { get { return DateTimeValue.Year; } }
+
+        public int Month { get { return DateTimeValue.Month; } }
+
+        public DateTime DateTimeValue { get; set; }
+
+        public decimal Income { get; set; }
+
+        public decimal Expenses { get; set; }
+
+        public decimal Total { get; set; }
+
+        public MonthlyLedger()
+        {
+
+        }
+
+        public MonthlyLedger(DateTime dateTime)
+        {
+            DateTimeValue = dateTime;
+        }
+
+    }
+}
